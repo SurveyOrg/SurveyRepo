4befdc5b64b30d5d62fbfe4aa2d783ce0aa3fc7a
morning3k <morning3k@qq.com>
2017-03-22 16:22:54 +0800

    增加实体对象repository 基本内容

---
+++ src/main/java/org/windbell_lab/survey/repo/repository/TcApplyRepository.java
@@ -0,0 +1,10 @@
+package org.windbell_lab.survey.repo.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by morning on 2017/3/21.
+ */
+@Repository
+public class TcApplyRepository {
+}
