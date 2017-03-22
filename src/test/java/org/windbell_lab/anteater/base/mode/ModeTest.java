f03c7cd9b1e9b58869b8ed9dbb31f0fd9f7b850a
morning3k <morning3k@qq.com>
2017-03-22 16:23:42 +0800

    增加单元测试内容

--- src/test/java/org/windbell_lab/anteater/base/mode/ModeTest.java
+++ src/test/java/org/windbell_lab/anteater/base/mode/ModeTest.java
@@ -1,19 +1,33 @@
 package org.windbell_lab.anteater.base.mode;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import org.windbell_lab.survey.repo.Main;
+import org.windbell_lab.survey.repo.mode.CmChannelEntity;
+import org.windbell_lab.survey.repo.repository.CmChannelRepository;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Main.class)
 
 public class ModeTest {
-
+	@Autowired
+	public CmChannelRepository	repository;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertNotNull(repository);
+		CmChannelEntity entity = new CmChannelEntity();
+		entity.setChannelName("ChannelName");
+		repository.save(entity);
 	}
 
 }
