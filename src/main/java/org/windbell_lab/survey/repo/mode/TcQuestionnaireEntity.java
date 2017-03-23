f99e0dda9fa62f1f8d3a129cd61a1fd6b6c74500
morning3k <morning3k@qq.com>
2017-03-23 15:09:54 +0800

    增加注释，删除实体基础类中含有的字段

--- src/main/java/org/windbell_lab/survey/repo/mode/TcQuestionnaireEntity.java
+++ src/main/java/org/windbell_lab/survey/repo/mode/TcQuestionnaireEntity.java
@@ -5,29 +5,20 @@ import java.sql.Timestamp;
 
 /**
  * Created by morning on 2017/3/20.
+ * 问券信息
  */
 @Entity
 @Table(name = "tc_questionnaire")
 public class TcQuestionnaireEntity extends BaseEntity {
 	private static final long serialVersionUID = 1595080357764490214L;
-	private long id;
+	/**
+	* questionCode : 问券编号
+	*/
 	private long questionCode;
+	/**
+	* questionName : 问券名称
+	*/
 	private String questionName;
-	private int delFlag;
-	private Long creator;
-	private Timestamp createTime;
-	private Long updater;
-	private Timestamp updateTime;
-
-	@Id
-	@Column(name = "id")
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
 
 	@Basic
 	@Column(name = "question_code")
@@ -49,56 +40,6 @@ public class TcQuestionnaireEntity extends BaseEntity {
 		this.questionName = questionName;
 	}
 
-	@Basic
-	@Column(name = "del_flag")
-	public int getDelFlag() {
-		return delFlag;
-	}
-
-	public void setDelFlag(int delFlag) {
-		this.delFlag = delFlag;
-	}
-
-	@Basic
-	@Column(name = "creator")
-	public Long getCreator() {
-		return creator;
-	}
-
-	public void setCreator(Long creator) {
-		this.creator = creator;
-	}
-
-	@Basic
-	@Column(name = "create_time")
-	public Timestamp getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(Timestamp createTime) {
-		this.createTime = createTime;
-	}
-
-	@Basic
-	@Column(name = "updater")
-	public Long getUpdater() {
-		return updater;
-	}
-
-	public void setUpdater(Long updater) {
-		this.updater = updater;
-	}
-
-	@Basic
-	@Column(name = "update_time")
-	public Timestamp getUpdateTime() {
-		return updateTime;
-	}
-
-	public void setUpdateTime(Timestamp updateTime) {
-		this.updateTime = updateTime;
-	}
-
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
@@ -107,22 +48,9 @@ public class TcQuestionnaireEntity extends BaseEntity {
 			return false;
 
 		TcQuestionnaireEntity that = (TcQuestionnaireEntity) o;
-
-		if (id != that.id)
-			return false;
 		if (questionCode != that.questionCode)
 			return false;
-		if (delFlag != that.delFlag)
-			return false;
 		if (questionName != null ? !questionName.equals(that.questionName) : that.questionName != null)
-			return false;
-		if (creator != null ? !creator.equals(that.creator) : that.creator != null)
-			return false;
-		if (createTime != null ? !createTime.equals(that.createTime) : that.createTime != null)
-			return false;
-		if (updater != null ? !updater.equals(that.updater) : that.updater != null)
-			return false;
-		if (updateTime != null ? !updateTime.equals(that.updateTime) : that.updateTime != null)
 			return false;
 
 		return true;
@@ -130,14 +58,9 @@ public class TcQuestionnaireEntity extends BaseEntity {
 
 	@Override
 	public int hashCode() {
-		int result = (int) (id ^ (id >>> 32));
+		int result = 0;
 		result = 31 * result + (int) (questionCode ^ (questionCode >>> 32));
 		result = 31 * result + (questionName != null ? questionName.hashCode() : 0);
-		result = 31 * result + delFlag;
-		result = 31 * result + (creator != null ? creator.hashCode() : 0);
-		result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
-		result = 31 * result + (updater != null ? updater.hashCode() : 0);
-		result = 31 * result + (updateTime != null ? updateTime.hashCode() : 0);
 		return result;
 	}
 }
